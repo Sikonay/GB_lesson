fcc2f6d34454863271d32f641316838995f095e2
Suiindik <kulumzhanov.s.b@gmail.com>
2021-10-03 22:24:34 +0600

    Задача 3

--- 3_lesson/3_1.py
+++ 3_lesson/3_1.py
@@ -1,2 +1,37 @@
-print(type(23))
-print(123)
+def repl(num):
+    if num == 'zero':
+        return 'ноль'
+    elif num == 'one':
+        return 'один'
+    elif num == 'two':
+        return 'два'
+    elif num == 'three':
+        return 'три'
+    elif num == 'four':
+        return 'четыре'
+    elif num == 'five':
+        return 'пять'
+    elif num == 'six':
+        return 'шесть'
+    elif num == 'seven':
+        return 'семь'
+    elif num == 'eight':
+        return 'восемь'
+    elif num == 'nine':
+        return 'девять'
+    elif num == 'ten':
+        return 'десять'
+    else:
+        print('введите на английском от 0 - 10')
+
+# для второго задания
+# def num_translate(num):
+
+#     if num == num.title():
+#         return repl(num.lower()).title()
+#     else:
+#         return repl(num.lower())
+
+
+num = input('напишите число на англииском ')
+print(num_translate(num))
