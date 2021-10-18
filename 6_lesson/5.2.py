5658fe219587cf1f6e53c880783f4cb459acf229
Your Name <you@example.com>
2021-10-18 17:46:44 +0600

    Задание 5.2 выполнил

---
+++ 6_lesson/5.2.py
@@ -0,0 +1,5 @@
+with open('nginx_logs.txt', 'r', encoding="utf-8") as f:
+    my_list = [line[:line.find(' ')] for line in f]
+
+adr = max(set(my_list), key=my_list.count)
+print(adr, my_list.count(adr))
