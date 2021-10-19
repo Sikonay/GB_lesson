783596f50811e14328a17d55e5043e29062b5b1d
Suiindik <kulumzhanov.s.b@gmail.com>
2021-10-19 21:19:51 +0600

    Выполнил задание 1

--- 7_lesson/7_1.py
+++ 7_lesson/7_1.py
@@ -1,27 +1,33 @@
 import os
 
-folders = ['my_project', ['settings', 'mainapp', 'adminapp', 'authapp']]
-url = ''
+folders = ['my_project', 'my_project/settings', 'my_project/mainapp', 'my_project/adminapp', 'my_project/authapp']
 
+for folder in folders:
+    if not os.path.exists(folder):
+        os.mkdir(folder)
 
-def recurs_folders(folder_list):
-    temp = []
-    recur = ''
-    for item in folder_list:
-        if isinstance(item, str):
-            # print(item)
-            if not os.path.isdir(item):
-                # папки нет надо ее создать и переитти к нему
-                os.mkdir(item)
-                recur = item
-            else:
-                # папка есть необходимо переитти к нему
-                os.chdir(item)
-        else:
-            # print(type(item))
-            os.chdir(recur)
-            recurs_folders(item)
-    return temp
+# folders = ['my_project', ['settings', 'mainapp', 'adminapp', 'authapp']]
 
-
-recurs_folders(folders)
+# def recurs_folders(folder_list):
+#     temp = []
+#     recur = ''
+#     for item in folder_list:
+#         if isinstance(item, str):
+#             # print(item)
+#             if not os.path.isdir(item):
+#                 # папки нет надо ее создать
+#                 os.mkdir(item)
+#                 # сохраняем
+#                 recur = item
+#             else:
+#                 # папка есть необходимо переитти к нему
+#                 os.chdir(item)
+#         else:
+#             # print(type(item))
+#
+#             os.chdir(recur)
+#             recurs_folders(item)
+#     return temp
+#
+#
+# recurs_folders(folders)
