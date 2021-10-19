d39be25b19d51f68527524dcebe2596c774ff57f
Your Name <you@example.com>
2021-10-19 16:58:12 +0600

    Задание 1 выполнил

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+folder = r'C:\Temp\some_data'
+# py_files = [item for item in os.listdir(folder)]
+# print(py_files)
+#
+# py_files = [item for item in os.listdir(folder)
+#             if item.lower().endswith('.py')]
+#
+# print(py_files)
+#
+# print(os.environ)
+#
+# py_files = [os.path.join(folder,item)
+#             for item in os.listdir(folder)]
+#
+# print(py_files)
+#
+# print(os.path.exists(r'C:\Temp\some_data\ackle.bin'))
+# print(os.path.exists(r'C:\Temp\some_data'))
+# print(os.path.exists(r'C:\Temp\some_data\acklesdfsd.bin'))
+#
+# print('-----------------------------\n')
+#
+# print(os.path.join(r'C:\Temp\some_data','ackle.bin'))
+# print(os.path.join(r'C:\Temp\some_data','acklesdfsd.bin'))
 
+# for root, dirs, files in os.walk(folder):
+#     print(root, dirs, files)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+try:
+    a = int('qwerty')
+    print('All ok')
+except:
+    print('error')
