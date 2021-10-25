019d4d9405103094e4c46e43770ee67dfa9ad0fe
Your Name <you@example.com>
2021-10-25 15:26:42 +0600

    Задание 2 выполнил

--- 8_lesson/8_2.py
+++ 8_lesson/8_2.py
@@ -2,13 +2,27 @@ import re
 
 
 def reg_parse(adres):
-    print(re.split(r' ', adres)[0])
-    print(adres)
-    print(re.search(r'[.*]', adres).group())
-    # print(re.split(r' ', adres)[3][1:])
+
+    # print(re.split(r' ', adres)[0])                     #   ip
+    # print(re.search(r'\[(.*?)\]', adres).group()[1:-1])       #   data
+    # print(re.search(r'([A-Z]{3})', adres).group())      #   GET
+    # print(re.search(r'\s(\/[\w\/]+)', adres).group()[1:])   #   path
+    # print(re.search(r'\s(\d{3})\s', adres).group()[1:])     #
+    # print(re.search(r'\d+ "', adres).group()[:-2])
+
+    remote_addr = re.split(r' ', adres)[0]
+    request_datetime = re.search(r'([A-Z]{3})', adres).group()
+    request_type = re.search(r'([A-Z]{3})', adres).group()
+    requested_resource = re.search(r'\s(\/[\w\/]+)', adres).group()[1:]
+    response_code = re.search(r'\s(\d{3})\s', adres).group()[1:]
+    response_size = re.search(r'\d+ "', adres).group()[:-2]
+
+    return (remote_addr,request_datetime, request_type, requested_resource, response_code, response_size)
+
 
 
 with open('../8_lesson/nginx_logs.txt', 'r', encoding="utf-8") as f:
     my_list = [reg_parse(line) for line in f]
 
-# print(my_list)
+for list_m in my_list:
+    print(list_m)
--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-def logger(verbosity=0):
-   def _logger(func):
-       def wrapper(*args):
-           result = func(*args)
-           msg = f'\tcall {func.__name__}'
-           if verbosity > 0:
-               msg = f'{msg}({", ".join(map(str, args))})'
-           if verbosity > 1:
-               msg = f'{msg} -> {result}'
-           return msg
+import re
 
-       return wrapper
+adres = '80.91.33.133 - - [04/Jun/2015:07:06:14 +0000] "GET /downloads/product_1 HTTP/1.1" 304 0 "-" "Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.22)"'
+adres2 ='192.235.75.62 - - [04/Jun/2015:07:06:41 +0000] "GET /downloads/product_1 HTTP/1.1" 404 334 "-" "Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.12)"'
 
-   return _logger
-
-
-@logger(2)
-def render_input(field):
-   return f'<input id="id_{field}" type="text" name="{field}">'
-
-username_f = render_input('username')
-password_f = render_input('password')
-print(username_f)
-print(password_f)
+print(adres)
+print(re.search(r'\d+ "', adres).group()[:-2])       #   data
+print(re.search(r'\d+ "', adres2).group()[:-2])       #   data
