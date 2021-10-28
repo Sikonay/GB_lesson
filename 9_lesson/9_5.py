60cea199d418b9984c09db14f0c1d10fcb67995e
Your Name <you@example.com>
2021-10-28 17:19:59 +0600

    Выполнил задание 5

---
+++ 9_lesson/9_5.py
@@ -0,0 +1,27 @@
+class Stationary:
+    title = "parent"
+
+    def draw(self):
+        print(f"Запуск отрисовки {self.title}")
+
+class Pen(Stationary):
+    title = "Pen"
+
+    def draw(self):
+        print(f"Запуск отрисовки {self.title}")
+
+class Pencil(Stationary):
+    title = "Pencil"
+
+    def draw(self):
+        print(f"Запсук отрисовки {self.title}")
+
+class Handle(Stationary):
+    title = "Handle"
+
+    def draw(self):
+        print(f"Запсук отрисовки {self.title}")
+
+a, b, c, d = Stationary(), Pen(), Pencil(), Handle()
+
+a.draw(), b.draw(), c.draw(), d.draw()
