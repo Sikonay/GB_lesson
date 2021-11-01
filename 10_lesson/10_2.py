9f675ffa291220bef39246f3cfb9ab707ac32eb2
Sikonay <kulumzhanov.s.b@gmail.com>
2021-11-01 16:28:33 +0600

    выполнил 2

--- 10_lesson/10_2.py
+++ 10_lesson/10_2.py
@@ -4,7 +4,7 @@ class Clothing(abc.ABC):
     def __init__(self, param):
         self.param = param
 
-    @abstractmethod
+    @abc.abstractmethod
     def consumption(self):
         pass
 class Coat(Clothing):
@@ -15,3 +15,8 @@ class Suit(Clothing):
     @property
     def consumption(self):
         return self.param * 2 + 0.3
+obj1 = Coat(48)
+obj2 = Suit(75)
+
+print(obj1.consumption)
+print(obj2.consumption)
